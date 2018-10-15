d4ad8736e3994537a3bf9aaa4f64657a1c066247
puikeicheng <puikeicheng@users.noreply.github.com>
2018-10-14 20:54:11 -0500

    Update Day1_pc.py

--- Day1/Day1_pc.py
+++ Day1/Day1_pc.py
@@ -1,3 +1,17 @@
-f = open('Day1_Data.txt','r')
-f = list(f.read())
-print(f.count('(')-f.count(')'))
+# Day 1
+f = list(open('Day1_Data.txt','r').read())
+
+# Floor landing
+floor = f.count('(')-f.count(')')
+print('Floor landing = %s' %floor)
+
+# Basement landing
+ud     = {'(':1,')':-1}
+lcount = 0
+for base,ll in enumerate(f):
+    if lcount > -1:
+        lcount += ud[ll]
+    else:
+        break
+    
+print('Basement landing = %s' %base)
